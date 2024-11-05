e381f20c0c96ccead53c525dc728ed25d77d2552
barearojo <barearojojuan@gmail.com>
2024-11-05 12:35:07 +0100

    hossam1522#14 Mas tallas añadidas
    Se ha realizado para tener aquellas consideradas estandar dentro del modelo. closes hossam1522#14

--- internal/models/talla.go
+++ internal/models/talla.go
@@ -3,9 +3,12 @@ package models
 type Talla string
 
 const (
-	XS Talla = "XS"
-	S  Talla = "S"
-	M  Talla = "M"
-	L  Talla = "L"
-	XL Talla = "XL"
+	XXS  Talla = "XXS"
+	XS   Talla = "XS"
+	S    Talla = "S"
+	M    Talla = "M"
+	L    Talla = "L"
+	XL   Talla = "XL"
+	XXL  Talla = "XXL"
+	XXXL Talla = "XXXL"
 )
