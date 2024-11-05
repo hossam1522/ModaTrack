cb4a4b5560cea9ca46cad155ca8449b450f3dbcd
barearojo <barearojojuan@gmail.com>
2024-11-05 12:46:53 +0100

    hossam1522#12 Creación constructor de la clase Venta
    Fecha sea aquella del momento que se ha creado así podemos hacer que solo haya fechas válidas o reciente. No busco establecer un límite por abajo por dos motivos. La ventas importantes serán a partir de laimplementación del sistema, segundo no sabemos desde cuando esta abierta la tienda así que no podemos saber cuando cortar por abajo. closes hossam1522#12

--- internal/models/venta.go
+++ internal/models/venta.go
@@ -6,3 +6,10 @@ type Venta struct {
 	fecha         time.Time
 	itemsVendidos []ProductoVendido
 }
+
+func NuevaVenta(itemsVendidos []ProductoVendido) Venta {
+	return Venta{
+		fecha:         time.Now(),
+		itemsVendidos: itemsVendidos,
+	}
+}
