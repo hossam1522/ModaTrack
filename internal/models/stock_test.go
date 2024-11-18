162d5c1bedf6dbbd5ad5327c969b395bb186ab64
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-18 22:57:45 +0100

    En relación a #24, se ha creado un test para la función para obtener ropa sin talla, por si queremos obtener las ropas con ese nombre sin importar la talla. Como da error ya que la función no lo tiene en cuenta, habrá que adaptar la función

--- internal/models/stock_test.go
+++ internal/models/stock_test.go
@@ -41,3 +41,29 @@ func TestStockConPrendaTalla(t *testing.T) {
 		t.Error("La talla no es la esperada")
 	}
 }
+
+// ¿Qué pasa si hay más de una prenda con el mismo nombre?
+// Debería devolver un vector con todas las prendas
+func TestStockConPrendaDuplicada(t *testing.T) {
+	stock := NewStock()
+	stock.inventario[Ropa{nombre: "camisa", precio: 10, talla: M}] = 1
+	stock.inventario[Ropa{nombre: "camisa", precio: 10, talla: L}] = 1
+
+	ropas, _ := stock.GetRopa("camisa")
+
+	if _, ok := interface{}(ropas).([]Ropa); !ok {
+		t.Errorf("Se esperaba un slice de Ropa, pero se obtuvo %T", ropas)
+	}
+
+	if len(ropas) != 2 {
+		t.Error("Se esperaban dos prendas")
+	}
+
+	if ropas[0].talla != M || ropas[1].talla != L {
+		t.Error("Las tallas no son las esperadas")
+	}
+
+	if ropas[0].nombre != "camisa" || ropas[1].nombre != "camisa" {
+		t.Error("Los nombres no son los esperados")
+	}
+}
