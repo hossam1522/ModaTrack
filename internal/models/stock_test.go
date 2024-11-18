49f8eec028ec45ad46e843ad3654b4732e360407
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-18 21:49:07 +0100

    En relación a #24, se ha creado un test para obtener una instancia Ropa de un nombre y talla específicas. Da fallo, falta implementar la función que hace esto

--- internal/models/stock_test.go
+++ internal/models/stock_test.go
@@ -25,3 +25,19 @@ func TestStockConPrenda(t *testing.T) {
 		t.Error("La prenda no es la esperada")
 	}
 }
+
+// Comprobar que puedo buscar una prenda por nombre y talla
+func TestStockConPrendaTalla(t *testing.T) {
+	stock := NewStock()
+	stock.inventario[Ropa{nombre: "camisa", precio: 10, talla: M}] = 1
+	ropa, err := stock.GetRopaTalla("camisa", M)
+	if err != nil {
+		t.Error("Se esperaba una prenda")
+	}
+	if ropa.nombre != "camisa" {
+		t.Error("La prenda no es la esperada")
+	}
+	if ropa.talla != M {
+		t.Error("La talla no es la esperada")
+	}
+}
