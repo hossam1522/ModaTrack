658f2fbd8370bb2518c664f93edd88a261dc3500
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-20 12:48:43 +0100

    En relación a #27, para comprobar que al crear una venta sin especificar una fecha coge la fecha actual, se comprueba si esto es así viendo la diferencia con la fecha actual y la fecha creada. Esa diferencia no debería ser de más unos milisegundos, pero se ha puesto un segundo para tirar por lo alto

--- internal/models/venta_test.go
+++ internal/models/venta_test.go
@@ -1,6 +1,9 @@
 package models
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 // Test para añadir una venta sin fecha
 func TestVenta(t *testing.T) {
@@ -10,5 +13,17 @@ func TestVenta(t *testing.T) {
 	venta := NuevaVenta(ropa)
 	if len(venta.itemsVendidos) != 1 {
 		t.Error("Se esperaba un item vendido")
+	}
+}
+
+// Test para comprobar que la fecha es correcta si no se especifica
+func TestVentaFechaActual(t *testing.T) {
+	ropa := map[Ropa]int{
+		{nombre: "camisa", precio: 10, talla: M}: 1,
+	}
+	venta := NuevaVenta(ropa)
+	// Si la diferencia es de más de un segundo, se considera incorrecto
+	if time.Until(venta.fecha) > time.Second {
+		t.Error("La fecha no es la actual")
 	}
 }
