01ee9348d45bbed3fdb4df49f09fa2568182da1c
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-21 09:15:34 +0100

    Se ha eliminado comentario de una función introducido en https://github.com/hossam1522/ModaTrack/pull/22/commits/e333da410140cf967cd5386c4ef6d68dd2315f06 por despiste

--- internal/models/venta.go
+++ internal/models/venta.go
@@ -10,27 +10,6 @@ type Venta struct {
 	itemsVendidos map[Ropa]int
 }
 
-/* func NuevaVenta(itemsVendidos map[Ropa]int, fecha ...time.Time) (Venta, error) {
-	fechaActual := time.Now()
-	fechaLimite := fechaActual.AddDate(-2, 0, 0)
-
-	var fechaVenta time.Time
-	if len(fecha) > 0 {
-		fechaVenta = fecha[0]
-		if fechaVenta.Before(fechaLimite) {
-			return Venta{}, errors.New("la fecha no puede ser anterior a dos años")
-		}
-	} else {
-		fechaVenta = fechaActual
-	}
-
-	return Venta{
-		fecha:         fechaVenta,
-		itemsVendidos: itemsVendidos,
-	}, nil
-}
-*/
-
 func NuevaVenta(itemsVendidos map[Ropa]int, inventario *Stock, fecha ...time.Time) (Venta, error) {
 	fechaActual := time.Now()
 	fechaLimite := fechaActual.AddDate(-2, 0, 0)
