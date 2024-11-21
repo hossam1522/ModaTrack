e333da410140cf967cd5386c4ef6d68dd2315f06
Hossam El Amraoui <hossam@correo.ugr.es>
2024-11-21 08:51:56 +0100

    En relación a #28 y a https://github.com/hossam1522/ModaTrack/pull/22/commits/a29e248b8c12b0489afd1a65314ba325889f9c7d, se ha añadido una referencia a un inventario en el constructor de venta. Hay que actualizar los tests que usan venta para esto

--- internal/models/venta.go
+++ internal/models/venta.go
@@ -10,7 +10,28 @@ type Venta struct {
 	itemsVendidos map[Ropa]int
 }
 
-func NuevaVenta(itemsVendidos map[Ropa]int, fecha ...time.Time) (Venta, error) {
+/* func NuevaVenta(itemsVendidos map[Ropa]int, fecha ...time.Time) (Venta, error) {
+	fechaActual := time.Now()
+	fechaLimite := fechaActual.AddDate(-2, 0, 0)
+
+	var fechaVenta time.Time
+	if len(fecha) > 0 {
+		fechaVenta = fecha[0]
+		if fechaVenta.Before(fechaLimite) {
+			return Venta{}, errors.New("la fecha no puede ser anterior a dos años")
+		}
+	} else {
+		fechaVenta = fechaActual
+	}
+
+	return Venta{
+		fecha:         fechaVenta,
+		itemsVendidos: itemsVendidos,
+	}, nil
+}
+*/
+
+func NuevaVenta(itemsVendidos map[Ropa]int, inventario *Stock, fecha ...time.Time) (Venta, error) {
 	fechaActual := time.Now()
 	fechaLimite := fechaActual.AddDate(-2, 0, 0)
 
