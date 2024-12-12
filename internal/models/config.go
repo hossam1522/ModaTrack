9986cc7649ff23064e36dd1293523e42678217d6
Hossam El Amraoui <hossam@correo.ugr.es>
2024-12-12 11:25:05 +0100

    En relación a #58, implementada función para leer de ficheros .env

--- internal/models/config.go
+++ internal/models/config.go
@@ -1,6 +1,11 @@
 package models
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/caarlos0/env/v11"
 )
 
@@ -13,5 +18,54 @@ func LoadConfig() (*Config, error) {
 	if err := env.Parse(cfg); err != nil {
 		return nil, err
 	}
+	return cfg, nil
+}
+
+func ReadConfigFromFile(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	os.Clearenv()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("línea inválida en .env: %s", line)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		os.Setenv(key, value)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func LoadConfigFromFile(file string) (*Config, error) {
+	err := ReadConfigFromFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := LoadConfig()
+
+	if err != nil {
+		return nil, err
+	}
+
 	return cfg, nil
 }
