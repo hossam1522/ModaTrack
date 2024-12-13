46d8828ddec9c38630f28db123b1943e568d597e
Hossam El Amraoui <hossam@correo.ugr.es>
2024-12-13 06:22:52 +0100

    En relación a #52 y a https://github.com/hossam1522/ModaTrack/pull/54\#issuecomment-2538846969, revert de https://github.com/hossam1522/ModaTrack/pull/54/commits/35f687a6dc6a8168b69c04e5fc52bb4c1d1ff160 creando archivo sobre la marcha

--- internal/models/config_test.go
+++ internal/models/config_test.go
@@ -32,3 +32,22 @@ func TestCargarConfiguracionPorDefecto(t *testing.T) {
 		t.Errorf("LoadConfig() LogFile = %v, debería ser ./logs/app.log", cfg.LogFile)
 	}
 }
+
+func TestCargarConfiguracionFichero(t *testing.T) {
+	envContent := "LOG_FILE=./logs/test.log\n"
+	tempFile := "./.env"
+	err := os.WriteFile(tempFile, []byte(envContent), 0644)
+	if err != nil {
+		t.Fatalf("No se pudo crear el archivo temporal .env.test: %v", err)
+	}
+	defer os.Remove(tempFile)
+
+	cfg, err := LoadConfigFromFile(tempFile)
+	if err != nil {
+		t.Errorf("LoadConfigFromFile() error = %v", err)
+		return
+	}
+	if cfg.LogFile != "./logs/test.log" {
+		t.Errorf("LoadConfigFromFile() LogFile = %v, debería ser ./logs/test.log", cfg.LogFile)
+	}
+}
