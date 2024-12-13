17f52cef253cd643352638974e922d42dc17169c
Hossam El Amraoui <hossam@correo.ugr.es>
2024-12-13 08:32:23 +0100

    En relación a #50, implementada la función de inicialización del logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	config "ModaTrack/internal/config"
+
+	"github.com/phuslu/log"
+)
+
+func init() {
+	cfg, err := config.LoadConfigFromFile(".env.test")
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error loading config")
+	}
+
+	logFile := cfg.LogFile
+
+	log.DefaultLogger = log.Logger{
+		Level:      log.InfoLevel,
+		Writer:     &log.FileWriter{Filename: logFile},
+		Caller:     1,
+		TimeField:  "date",
+		TimeFormat: "2006-01-02",
+	}
+}
