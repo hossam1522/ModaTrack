1827ebc69bb8c40717e910cd2376b386de4bbc46
Hossam El Amraoui <hossam@correo.ugr.es>
2024-12-20 10:53:05 +0100

    En relación a #50 y a lo comentado en clase, se ha hecho que los logs se escriban tanto por consola como por archivo

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -17,12 +17,15 @@ func InitLogger() {
 
 	log.DefaultLogger = log.Logger{
 		Level: log.InfoLevel,
-		Writer: &log.FileWriter{Filename: logFile,
-			FileMode:     0600,
-			MaxSize:      100 * 1024 * 1024,
-			MaxBackups:   7,
-			EnsureFolder: true,
-			LocalTime:    true},
+		Writer: &log.MultiEntryWriter{
+			&log.FileWriter{Filename: logFile,
+				FileMode:     0600,
+				MaxSize:      100 * 1024 * 1024,
+				MaxBackups:   7,
+				EnsureFolder: true,
+				LocalTime:    true},
+			&log.ConsoleWriter{ColorOutput: true},
+		},
 		Caller:     1,
 		TimeField:  "date",
 		TimeFormat: "2006-01-02",
