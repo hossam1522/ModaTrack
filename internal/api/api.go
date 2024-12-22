3b966e65de1fc386bb8722715befc96b6fcbcbdc
Hossam El Amraoui <hossam@correo.ugr.es>
2024-12-22 22:48:09 +0100

    En relación a #62 y a #2, metidas las rutas para obtener una prenda o la talla de una prenda

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,18 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 )
 
 var router = chi.NewRouter()
+
+func init() {
+	router.Get("/prenda/{id}", GetPrenda)
+	router.Get("/prenda/{id}/{talla}", GetPrendaTalla)
+}
+
+func GetPrenda(w http.ResponseWriter, r *http.Request) {}
+
+func GetPrendaTalla(w http.ResponseWriter, r *http.Request) {}
