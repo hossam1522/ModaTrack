91eb369259c80f69ece7db5dbfac26a66c5f4b8f
Hossam El Amraoui <hossam@correo.ugr.es>
2025-01-02 00:52:24 +0100

    En relación a #67, se ha implementado un test con una BD de prueba para poder implementar la ruta de obtener prendas sin indicar talla

--- internal/api/api_test.go
+++ internal/api/api_test.go
@@ -1,18 +1,44 @@
 package api
 
 import (
+	"ModaTrack/internal/models"
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
 )
 
+func BD_prueba() *models.BD {
+	bd := models.NewBD()
+
+	bd.InsertarRopa("camisa", models.M, 1)
+	bd.InsertarRopa("camisa", models.L, 1)
+	bd.InsertarRopa("pantalon", models.M, 1)
+	bd.InsertarRopa("pantalon", models.L, 1)
+
+	return bd
+}
+
 func TestGetPrendas(t *testing.T) {
 	router := chi.NewRouter()
 	server := httptest.NewServer(router)
 	defer server.Close()
 
-	//stock := models.NewStock()
-	//stock.inventario[models.Ropa{Nombre: "camisa", Talla: models.M}] = 1
-
+	url := server.URL + "/prendas/camisa"
+	resp, err := server.Client().Get(url)
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Se esperaba un status 200, se obtuvo %d", resp.StatusCode)
+	}
+	var prendas []models.Ropa
+	err = json.NewDecoder(resp.Body).Decode(&prendas)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(prendas) != 2 {
+		t.Errorf("Se esperaban 2 prendas, se obtuvieron %d", len(prendas))
+	}
 }
