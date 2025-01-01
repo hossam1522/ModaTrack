eacf232b80c0f5c632782b5ebf69064925ada90e
Hossam El Amraoui <hossam@correo.ugr.es>
2025-01-01 22:24:09 +0100

    En relación a #70 y a https://github.com/hossam1522/ModaTrack/pull/68/commits/ebbbe3d2428ba2e5dcca5880f7aa2584c24ab37f, se ha implementado la función para pasar dicho test

--- internal/models/bd.go
+++ internal/models/bd.go
@@ -1,6 +1,31 @@
 package models
 
+import (
+	"ModaTrack/internal/log"
+	"errors"
+)
+
 type BD struct {
 	stock  *Stock
 	ventas []Venta
+}
+
+func NewBD() *BD {
+	return &BD{
+		stock:  NewStock(),
+		ventas: []Venta{},
+	}
+}
+
+func (bd *BD) InsertarRopa(nombre string, talla Talla, cantidad int) error {
+	log.GetLogger().Info().Msg("Insertando ropa en la base de datos")
+
+	cantidadActual := bd.stock.inventario[Ropa{nombre: nombre, talla: talla}]
+	bd.stock.inventario[Ropa{nombre: nombre, talla: talla}] = cantidadActual + cantidad
+
+	if bd.stock.inventario[Ropa{nombre: nombre, talla: talla}] != cantidadActual+cantidad {
+		return errors.New("no se ha podido insertar la ropa")
+	}
+
+	return nil
 }
