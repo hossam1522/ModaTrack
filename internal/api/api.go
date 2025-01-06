d9134de8332e00a6e00ba76d3629b538e2c3b638
Hossam El Amraoui <hossam@correo.ugr.es>
2025-01-06 02:53:14 +0100

    En relación a https://github.com/hossam1522/ModaTrack/pull/68\#discussion_r1903092968, eliminado el código repetido a la hora de pillar los parámetros de la ruta añadiendo un middleware que coge del contexto los parámetros, los asigna a un map, este map se añade al contexto y luego se pueden coger los parámetros directamente de este contexto sin necesidad de estar declarando los parámetros explícitamente para trabajar con ellos haciendo uso de chi.URLParam

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"ModaTrack/internal/models"
+	"context"
 	"encoding/json"
 	"net/http"
 	"strconv"
@@ -13,21 +14,33 @@ import (
 var router = chi.NewRouter()
 var bd = models.GetBDPrueba()
 
+func capturarParametrosRuta(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parametrosRuta := chi.RouteContext(r.Context()).URLParams
+		parametrosMap := make(map[string]string, len(parametrosRuta.Keys))
+		for i, key := range parametrosRuta.Keys {
+			parametrosMap[key] = parametrosRuta.Values[i]
+		}
+		ctx := context.WithValue(r.Context(), "parametrosMap", parametrosMap)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func getRouter() *chi.Mux {
-	router.Get("/prendas/{nombre}", getPrendas)
-	router.Get("/prendas/{nombre}/{talla}", getPrendaTalla)
-	router.Post("/prendas/{nombre}/{talla}/{cantidad}", postPrendaTalla)
-	router.Get("/prendas/{nombre}/{talla}/ventas", getVentasPrenda)
-	router.Get("/prendas/{nombre}/{talla}/ventas/{fecha}", getVentaFecha)
-	router.Put("/prendas/{nombre}/{talla}/ventas/{fecha}", putVenta)
-	router.Delete("/prendas/{nombre}/{talla}/ventas/{fecha}", deleteVenta)
+	router.With(capturarParametrosRuta).Get("/prendas/{nombre}", getPrendas)
+	router.With(capturarParametrosRuta).Get("/prendas/{nombre}/{talla}", getPrendaTalla)
+	router.With(capturarParametrosRuta).Post("/prendas/{nombre}/{talla}/{cantidad}", postPrendaTalla)
+	router.With(capturarParametrosRuta).Get("/prendas/{nombre}/{talla}/ventas", getVentasPrenda)
+	router.With(capturarParametrosRuta).Get("/prendas/{nombre}/{talla}/ventas/{fecha}", getVentaFecha)
+	router.With(capturarParametrosRuta).Put("/prendas/{nombre}/{talla}/ventas/{fecha}", putVenta)
+	router.With(capturarParametrosRuta).Delete("/prendas/{nombre}/{talla}/ventas/{fecha}", deleteVenta)
 
 	return router
 }
 
 func getPrendas(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	prendas, err := models.ObtenerPrenda(bd, nombre)
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	prendas, err := models.ObtenerPrenda(bd, params["nombre"])
 	if err != nil {
 		http.Error(w, "No se ha podido obtener la prenda de la base de datos", http.StatusNotFound)
 		return
@@ -40,9 +53,8 @@ func getPrendas(w http.ResponseWriter, r *http.Request) {
 }
 
 func getPrendaTalla(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	prenda, err := models.ObtenerPrendaTalla(bd, nombre, models.Talla(talla))
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	prenda, err := models.ObtenerPrendaTalla(bd, params["nombre"], models.Talla(params["talla"]))
 	if err != nil {
 		http.Error(w, "No se ha podido obtener la prenda de la base de datos", http.StatusNotFound)
 		return
@@ -55,15 +67,13 @@ func getPrendaTalla(w http.ResponseWriter, r *http.Request) {
 }
 
 func postPrendaTalla(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	cantidad := chi.URLParam(r, "cantidad")
-	cantidadInt, errNum := strconv.Atoi(cantidad)
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	cantidadInt, errNum := strconv.Atoi(params["cantidad"])
 	if errNum != nil {
 		http.Error(w, "La cantidad debe ser un número entero", http.StatusBadRequest)
 		return
 	}
-	err := models.InsertarRopa(bd, nombre, models.Talla(talla), cantidadInt)
+	err := models.InsertarRopa(bd, params["nombre"], models.Talla(params["talla"]), cantidadInt)
 	if err != nil {
 		http.Error(w, "No se ha podido insertar la prenda en la base de datos", http.StatusBadRequest)
 		return
@@ -72,9 +82,8 @@ func postPrendaTalla(w http.ResponseWriter, r *http.Request) {
 }
 
 func getVentasPrenda(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	ventas, err := models.ObtenerVentas(bd, nombre, models.Talla(talla))
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	ventas, err := models.ObtenerVentas(bd, params["nombre"], models.Talla(params["talla"]))
 	if err != nil {
 		http.Error(w, "No se han podido obtener las ventas de la prenda", http.StatusNotFound)
 		return
@@ -87,15 +96,13 @@ func getVentasPrenda(w http.ResponseWriter, r *http.Request) {
 }
 
 func getVentaFecha(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	fecha := chi.URLParam(r, "fecha")
-	fechaVenta, err := time.Parse(time.RFC3339, fecha)
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	fechaVenta, err := time.Parse(time.RFC3339, params["fecha"])
 	if err != nil {
 		http.Error(w, "La fecha no tiene el formato correcto, debe ser RFC3339", http.StatusUnprocessableEntity)
 		return
 	}
-	venta, err := models.ObtenerVentas(bd, nombre, models.Talla(talla), fechaVenta)
+	venta, err := models.ObtenerVentas(bd, params["nombre"], models.Talla(params["talla"]), fechaVenta)
 	if err != nil {
 		http.Error(w, "No se ha podido obtener la venta de la prenda de la base de datos", http.StatusNotFound)
 		return
@@ -108,15 +115,13 @@ func getVentaFecha(w http.ResponseWriter, r *http.Request) {
 }
 
 func putVenta(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	fecha := chi.URLParam(r, "fecha")
-	fechaVenta, err := time.Parse(time.RFC3339, fecha)
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	fechaVenta, err := time.Parse(time.RFC3339, params["fecha"])
 	if err != nil {
 		http.Error(w, "La fecha no tiene el formato correcto, debe ser RFC3339", http.StatusUnprocessableEntity)
 		return
 	}
-	err = models.InsertarVenta(bd, nombre, models.Talla(talla), fechaVenta)
+	err = models.InsertarVenta(bd, params["nombre"], models.Talla(params["talla"]), fechaVenta)
 	if err != nil {
 		http.Error(w, "No se ha podido insertar la venta en la base de datos", http.StatusBadRequest)
 		return
@@ -125,15 +130,13 @@ func putVenta(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteVenta(w http.ResponseWriter, r *http.Request) {
-	nombre := chi.URLParam(r, "nombre")
-	talla := chi.URLParam(r, "talla")
-	fecha := chi.URLParam(r, "fecha")
-	fechaVenta, err := time.Parse(time.RFC3339, fecha)
+	params := r.Context().Value("parametrosMap").(map[string]string)
+	fechaVenta, err := time.Parse(time.RFC3339, params["fecha"])
 	if err != nil {
 		http.Error(w, "La fecha no tiene el formato correcto, debe ser RFC3339", http.StatusUnprocessableEntity)
 		return
 	}
-	err = models.EliminarVenta(bd, nombre, models.Talla(talla), fechaVenta)
+	err = models.EliminarVenta(bd, params["nombre"], models.Talla(params["talla"]), fechaVenta)
 	if err != nil {
 		http.Error(w, "No se ha podido eliminar la venta de la base de datos", http.StatusBadRequest)
 		return
